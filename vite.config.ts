201e14ea4108762657ad9cfb2aaf874b2bc2b7b3
choudharysunaina <choudhary.sunaina09@gmail.com>
2025-08-16 18:09:44 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/kuno/", // 👈 IMPORTANT: your repo name
+  base: "/kuno/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
